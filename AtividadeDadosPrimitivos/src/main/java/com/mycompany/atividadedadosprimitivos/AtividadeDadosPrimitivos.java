1cce17d42c039a33a8c04faed2ce32bb4217ba93
marianaqueirozz <mariana.queiroz20@etec.sp.gov.br>
2025-04-09 13:51:19 -0300

    Add files via upload

---
+++ AtividadeDadosPrimitivos/src/main/java/com/mycompany/atividadedadosprimitivos/AtividadeDadosPrimitivos.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.mycompany.atividadedadosprimitivos;
+
+/**
+ *
+ * @author FATEC ZONA LESTE
+ */
+public class AtividadeDadosPrimitivos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {/**primeiro programa em java*/
+/**Eu Mariana Pereira estou desenvolvendo meu primeiro algoritimo*/
+            // Declaração de variáveis primitivaas
+           byte faixaetaria = 25; //Armazena algarismos pequenos (-128 a 127)
+            short ano = 2025; //Armazena valores um pouco maiores (-32.76
+            int populacaoMunicipio = 500000; // Tipos mais comuns para números
+            long populacaoMundial = 8000000000L; //
+            
+            float altura = 1.75f; // vai receber float 
+            double peso = 72.5;  //Mais preciso que o float usado para distinguir
+            
+            char inicial = 'J';// Armazena um único caractere
+            boolean ativo = true; //Armazena se é verdadeiro ou falso
+            
+            //Agora vamos fazer o algoritimo para exibir os valores da variável
+            System.out.println("Faixa etária: " + faixaetaria);
+            System.out.println("Ano: " + ano);
+            System.out.println("População do município: " + populacaoMunicipio);
+            System.out.println("População Mundial: " + populacaoMundial);
+            System.out.println("Altura: " + altura + "m");
+            System.out.println("Peso: " + peso + "kg" );
+            System.out.println("Inicial: " + inicial);
+            System.out.println("Está ativo? " + ativo);
+                
+  }
+        // TODO code application logic here
+    }
+    
